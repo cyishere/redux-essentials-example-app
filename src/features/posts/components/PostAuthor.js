d4d48beeae2998ef9ce836f6ae9f2c59752b2ea7
irini <cyishere@gmail.com>
2020-11-07 12:33:39 +0800

    Add user pages

--- src/features/posts/components/PostAuthor.js
+++ src/features/posts/components/PostAuthor.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const PostAuthor = ({ userId }) => {
   const author = useSelector((state) =>
     state.users.find((user) => user.id === userId)
   );
-  return <span>by {author ? author.name : "Unknown author"}</span>;
+  return (
+    <span>
+      by{" "}
+      <Link to={`/users/${author.id}`}>
+        {author ? author.name : "Unknown author"}
+      </Link>
+    </span>
+  );
 };
 
 export default PostAuthor;
---
+++ src/features/users/UserPage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { selectUserById } from "./usersSlice";
+import { selectAllPosts } from "../posts/postsSlice";
+
+const UserPage = ({ match }) => {
+  const { userId } = match.params;
+
+  const user = useSelector((state) => selectUserById(state, userId));
+
+  const postsByUser = useSelector((state) => {
+    const allPosts = selectAllPosts(state);
+    return allPosts.filter((post) => post.user === userId);
+  });
+
+  const postTitles = postsByUser.map((post) => (
+    <li key={post.id}>
+      <Link to={`/posts/${post.id}`}>{post.title}</Link>
+    </li>
+  ));
+
+  return (
+    <section>
+      <h2>{user.name}</h2>
+      <ul>{postTitles}</ul>
+    </section>
+  );
+};
+
+export default UserPage;
