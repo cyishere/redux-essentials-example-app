3b2f370682f1117b63dcc19dd5e150feb9495e71
irini <cyishere@gmail.com>
2020-11-07 10:25:58 +0800

    Fetch all users from API

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,15 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { client } from "../../api/client";
 
-const initialState = [
-  { id: "0", name: "Debbie Ocean" },
-  { id: "1", name: "Lou Miller" },
-  { id: "2", name: "Tammy" },
-];
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await client.get("/fakeApi/users");
+  return response.users;
+});
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchUsers.fulfilled]: (state, action) => {
+      return action.payload;
+    },
+  },
 });
 
 export default usersSlice.reducer;
